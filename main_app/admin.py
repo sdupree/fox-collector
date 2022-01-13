665c4d5f2bc7b5b741c5b9cb623654ae5caac011
Shane Dupree <shane.dupree@gmail.com>
2022-01-13 17:28:37 -0500

    Add database backend and admin functionality.

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Fox
 
 # Register your models here.
+admin.site.register(Fox)
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Fox(models.Model):
+  name = models.CharField(max_length=20)
+  species = models.CharField(max_length=20)
+  description = models.TextField(max_length=250)
+  age = models.IntegerField()
+  def __str__(self):
+    return self.name
